12bc0184487350b73ac53c808aefe9b6323d37d2
StellaCorrea <ster_muniz@hotmail.com>
2025-09-03 15:29:12 -0300

    ajuste cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     video: true, // habilita gravação de vídeo
+    trashAssetsBeforeRuns: false, // ISSO EVITA QUE OS VÍDEOS SEJAM DELETADOS
     videosFolder: 'cypress/videos', // pasta onde os vídeos serão salvos
     screenshotsFolder: 'cypress/screenshots' // se quiser capturar falhas também
   }
