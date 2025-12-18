a2ef2f3938d6d6db5a61dffd277081cf39c60b08
Stella Muniz Corrêa <101405223+StellaCorrea@users.noreply.github.com>
2025-12-17 21:20:02 -0300

    ajuste config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,19 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
-    specPattern: 'cypress/e2e/**/*.cy.js', // Permite subpastas em e2e/
-    video: true, // Ativa gravação de vídeo
-    trashAssetsBeforeRuns: false, // NÃO apaga os vídeos antigos
-    videosFolder: 'cypress/videos', // Pasta padrão para vídeos
-    screenshotsFolder: 'cypress/screenshots' // Pasta para screenshots (em falhas ou chamadas explícitas)
-  }
-});
+    specPattern: 'cypress/e2e/**/*.cy.js',
+
+    setupNodeEvents(on, config) {
+      // eventos do node (pode ficar vazio)
+      return config
+    },
+
+    video: true,
+    trashAssetsBeforeRuns: false,
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
+  },
+})
+
